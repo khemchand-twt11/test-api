78cd51423afab1cdc25abf1b75df457ddb3b845a
khemchand-twt11 <gauravsharma6290852476@gmail.com>
2023-02-21 15:43:59 +0530

    data add

---
+++ file.js
@@ -0,0 +1,46 @@
+// {
+//     id:"id",
+//     title:"product_title",
+//     image1: "url",
+//     image2:"url",
+//     image3:"url",
+//     actualValue:"actual_price",
+//     discountValue:"price after discount",
+//     discount:"discount percentage",
+//     size: "xs/s/l/xl,xxl",
+//     ocassion: "cloth_according to occasion",
+//     deals: "bu1Get2/doorbuster/shopsale",
+//     category: "bottoms/tops/pajamas/shoes/accessories/sets",
+//     brand:"clothie's/oshKosh",
+//     type:"baby_boy/baby_girl/toddler_boy/toddler_girl/girl/boy"
+//   }
+
+// {
+//     id:"id",
+//     title:"product_title",
+//     image1: "url",
+//     image2:"url",
+//     image3:"url",
+//     actualValue:"actual_price",
+//     discountValue:"price after discount",
+//     discount:"discount percentage",
+//     size: "xs/s/l/xl,xxl",
+//     ocassion: "cloth_according to occasion",
+//     deals: "bu1Get2/doorbuster/shopsale",
+//     category: "bottoms/tops/pajamas/shoes/accessories/sets",
+//     brand:"clothie's/oshKosh"
+//   }
+
+// "title":"Baby 2-Piece Bodysuit Pant Set",
+// "image1": "https://cdn-fsly.yottaa.net/578855e32bb0ac10350004f8/www.carters.com/v~4b.2a5/dw/image/v2/AAMK_PRD/on/demandware.static/-/Sites-carters_master_catalog/default/dwcb491c06/productimages/1O617610.jpg?sw=354&sh=444&yocs=g_j_",
+// "image2":"https://cdn-fsly.yottaa.net/578855e32bb0ac10350004f8/www.carters.com/v~4b.2a5/dw/image/v2/AAMK_PRD/on/demandware.static/-/Sites-carters_master_catalog/default/dw7f69d909/productimages/1O617610_LV03.jpg?sw=354&sh=444&yocs=g_j_",
+
+// "actualPrice":"22.00",
+// "discountPrice":"9.00",
+// "discount":"59% off",
+// "size": "xs",
+// "ocassion": "",
+// "deals": "shopsale",
+// "category": "sets",
+// "brand":"clothie's/oshKosh",
+// "type":"baby_girl"
